543deb631557e3c485352fc3f8d2d50448fc8a67
Sergio Rosa <scarlosjazz@gmail.com>
2020-12-02 18:10:02 -0500

    routes - pages

---
+++ src/pages/repository/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Repository: React.FC = () => {
+  return <h1>Hello Dash</h1>;
+};
+
+export default Repository;
---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/repository';
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/repository" component={Repository} />
+    </Switch>
+  );
+};
+
+export default Routes;
